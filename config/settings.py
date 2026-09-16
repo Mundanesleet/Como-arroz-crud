574380cbffae9aefe755297a0ce871908df44e0d
mundanesleet <jalex17nov@outlook.com>
2026-09-16 18:15:26 -0500

    Configurar archivos estaticos para produccion

--- config/settings.py
+++ config/settings.py
@@ -90,6 +90,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -204,6 +205,7 @@ STATICFILES_DIRS = [
 # para producción.
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # ============================================================
 # MEDIA FILES
